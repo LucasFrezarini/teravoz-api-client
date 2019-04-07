a08582a58e6a35250857fd18d99f1b14b98ecdea
Lucas Frezarini <lucas.frezarini@gmail.com>
2019-04-07 18:28:02 -0300

    Creating the base of client API using Node, Typescript and GraphQL

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = function(api) {
+  api.cache(true);
+
+  const presets = ["@babel/env", "@babel/preset-typescript"];
+  const plugins = [
+    "@babel/proposal-class-properties",
+    "@babel/proposal-object-rest-spread",
+  ];
+
+  return {
+    presets,
+    plugins,
+  };
+};
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { GraphQLServer } from "graphql-yoga";
+
+import Query from "./resolvers/Query";
+
+const resolvers = {
+  Query,
+};
+
+const server = new GraphQLServer({
+  resolvers,
+  typeDefs: "./src/schema.graphql",
+});
+
+server.start(() => {
+  console.log("Server running at port 4000");
+});
