42148a2edd7a8a5b11583477bf450ef6d5836b1e
Lucas Frezarini <lucas.frezarini@gmail.com>
2019-04-07 20:55:16 -0300

    WIP: saving new calls into the JSON file

--- src/handlers/webhook.ts
+++ src/handlers/webhook.ts
@@ -1,7 +1,20 @@
 import { Request, Response } from "express";
 
-const webhookHandler = (req: Request, res: Response): void => {
-  res.status(200).send();
+import CallService from "@services/CallService";
+
+const webhookHandler = async (req: Request, res: Response): Promise<void> => {
+  const test = await CallService.newCall({
+    type: "call.new",
+    call_id: "1463669263.30033",
+    code: "123456",
+    direction: "inbound",
+    our_number: "0800000000",
+    their_number: "11999990000",
+    their_number_type: "mobile",
+    timestamp: "2017-01-01T00:00:00Z",
+  });
+
+  res.status(200).json(JSON.parse(test));
 };
 
 export default webhookHandler;
--- src/services/CallService.ts
+++ src/services/CallService.ts
@@ -1,7 +1,26 @@
+import path from "path";
+import fs from "fs";
+import { promisify } from "util";
+
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+
+const jsonPath = path.join(__dirname, "..", "data", "calls.json");
 class CallService {
-  public newCall(data: any): void {
-    console.log("Not implemented yet");
+  public async newCall(call: any): Promise<any> {
+    const data = await readFile(jsonPath, "utf8");
+    const obj = JSON.parse(data.toString());
+
+    if (!obj.data) {
+      obj.data = [];
+    }
+
+    obj.data.push(call);
+
+    const json = JSON.stringify(obj);
+    await writeFile(jsonPath, json, "utf8");
+    return json;
   }
 }
 
-export default CallService;
+export default new CallService();
