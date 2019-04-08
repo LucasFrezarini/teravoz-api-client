0967658e26298eaab68a64183e3ff8a48d527d1c
Lucas Frezarini <lucas.frezarini@gmail.com>
2019-04-07 22:25:31 -0300

    Calling the right methods depending on what type the webhook will send on the call

--- src/handlers/webhook.ts
+++ src/handlers/webhook.ts
@@ -4,9 +4,22 @@ import CallService from "@services/CallService";
 
 const webhookHandler = async (req: Request, res: Response): Promise<void> => {
   try {
-    const test = await CallService.newCall(req.body);
+    const call = req.body;
+    const { type } = call;
 
-    res.status(200).json(JSON.parse(test));
+    switch (type) {
+      case "call.new":
+        await CallService.newCall(call);
+        break;
+      case "call.standby":
+        await CallService.standBy(call);
+        break;
+      default:
+        await CallService.update(call);
+        break;
+    }
+
+    res.status(200).json({ status: "ok " });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal server error" });
--- src/services/CallService.ts
+++ src/services/CallService.ts
@@ -7,30 +7,80 @@ const writeFile = promisify(fs.writeFile);
 
 const jsonPath = path.join(__dirname, "..", "data", "data.json");
 class CallService {
+  public constructor() {
+    const data = fs.readFileSync(jsonPath, "utf8");
+    const obj = JSON.parse(data);
+    let shouldUpdate = false;
+
+    if (!obj.calls) {
+      obj.calls = [];
+      shouldUpdate = true;
+    }
+    if (!obj.contacts) {
+      obj.contacts = [];
+      shouldUpdate = true;
+    }
+
+    if (shouldUpdate) {
+      const json = JSON.stringify(obj);
+      fs.writeFileSync(jsonPath, json, "utf8");
+    }
+  }
   public async newCall(call: any): Promise<any> {
-    const data = await readFile(jsonPath, "utf8");
-    const obj = JSON.parse(data.toString());
+    const obj = await this.getData();
+
+    obj.calls.push(call);
+
+    return this.setData(obj);
+  }
+
+  public async standBy(call: any): Promise<any> {
+    const obj = await this.getData();
 
     // eslint-disable-next-line @typescript-eslint/camelcase
     const { their_number } = call;
 
-    if (!obj.contacts) {
-      obj.contacts = [];
-    }
-
-    if (!obj.calls) {
-      obj.calls = [];
-    }
+    obj.calls = this.updateCall(obj.calls, call);
 
     if (!obj.contacts.includes(their_number)) {
       obj.contacts.push(their_number);
+      // Post to /actions with extension 900
+    } else {
+      // post to /actions with extension 901
     }
 
-    obj.calls.push(call);
+    return this.setData(obj);
+  }
 
-    const json = JSON.stringify(obj);
-    await writeFile(jsonPath, json, "utf8");
-    return json;
+  public async update(call: any): Promise<any> {
+    const obj = await this.getData();
+
+    obj.calls = this.updateCall(obj.calls, call);
+
+    return this.setData(obj);
+  }
+
+  private async getData(): Promise<any> {
+    const data = await readFile(jsonPath, "utf8");
+    return JSON.parse(data.toString());
+  }
+
+  private async setData(data: any): Promise<any> {
+    const json = JSON.stringify(data);
+    return writeFile(jsonPath, json, "utf8");
+  }
+
+  private updateCall(history: any[], call: any): any[] {
+    const index = history.findIndex(
+      (registeredCall: any): any => registeredCall.call_id === call.call_id,
+    );
+
+    if (index === -1) {
+      throw new Error(`Call with id ${call.call_id} not found.`);
+    }
+
+    history[index] = call;
+    return history;
   }
 }
 
