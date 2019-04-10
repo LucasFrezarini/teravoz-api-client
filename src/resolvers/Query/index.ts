f2b72b5e78d551f876b640e7aa9fdabb1a1a1776
Lucas Frezarini <lucas.frezarini@gmail.com>
2019-04-09 23:38:25 -0300

    Reverse ordering the calls array and filtering finished calls

--- src/resolvers/Query/index.ts
+++ src/resolvers/Query/index.ts
@@ -1,5 +1,6 @@
 import { readFile } from "@utils/fs-promise";
 import path from "path";
+import { Call } from "@interfaces/Data";
 
 const jsonPath = path.join(__dirname, "..", "..", "data", "data.json");
 
@@ -8,7 +9,9 @@ const Query = {
     const data = await readFile(jsonPath, "utf8");
     const obj = JSON.parse(data);
 
-    return obj.calls;
+    return obj.calls
+      .filter((call: Call): boolean => call.type !== "call.finished")
+      .reverse();
   },
 
   async contacts(): Promise<any> {
